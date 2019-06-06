e9f65d7554d501821edefa864b9ba08386f35480
pixeldiebe <48907910+pixeldiebe@users.noreply.github.com>
2019-06-06 21:31:09 +0200

    vertikale Animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,83 @@
+// ------------------ Create the Elements --------------------
+const wrapperElem = document.querySelector('.anime-wrapper');
+const numberOfdivs = 5;
+
+function createElem() {
+  let elem = document.createElement('div');
+  //elem.classList.add('expand');
+  elem.setAttribute("class", "expand expand-js-1");
+  wrapperElem.appendChild(elem);
+}
+for(let i = 0; i < numberOfdivs; i++) createElem();
+
+// ------------------ Header Animation -----------------------
+var colors = ["#f5db61","#eeffac", "#a7c26a", "#77882f", "#8e2800"];
+var colors2 = ["#fff0a5", "#b4beaf","#6c9577", "#486051", "#404940"];
+
+var expandBox = anime({
+  targets: '.expand-js-1',
+  duration: 1300,
+  //direction: 'reverse',
+  width: [ anime.stagger(['20%','100%'])],
+  //width: '100%',
+  easing: 'easeInOutSine',
+  skewX: [ anime.stagger([-120, -20]) , 0 ],
+  delay: anime.stagger(300),
+  background: function(el, i, l){
+    return colors2[i];
+  },
+});
+
+
+// --------------- Run Animation by trigger ----------------------------
+function runbox2() {
+
+  let directionSwitch = 'normal';
+
+  var expandBox2 = anime({
+    targets: '.expand-js-2',
+    direction: directionSwitch,
+    duration: 1500,
+    easing: 'easeInOutSine',
+    skewY: [-90,0],
+    opacity: [0,1],
+    delay: anime.stagger(400),
+    background: function(el, i, l){
+      return colors[i];
+    },
+    autoplay: true,
+  });
+
+}
+
+
+
+// --------------- Animate the button ----------------------------
+//document.getElementById('gobutton').onmouseover = mytest();
+document.getElementById('gobutton').addEventListener("mouseover", vibrate );
+
+function mytest() {
+  console.log("success");
+}
+
+function vibrate() {
+
+  console.log("mouse-over");
+
+  anime({
+    targets: '#gobutton',
+    translateX: ['-.25rem', '.25rem'],
+    duration: 50,
+    direction: 'alternate',
+    loop: 10,
+    easing: 'easeOutBack',
+  });
+
+}
+
+// ---------------- Test Animation for fun ----------------------
 
 //anime animation
-
 var blocksAnimation = anime({
     targets: '.js-box',
     direction: 'normal',
@@ -26,58 +103,3 @@ var blocksAnimation = anime({
     delay: (elm, i)  => i * 100,
     //loop: true,
 });
-
-var colors = ["#FF6138","#2980B9", "#282741", "#FFBE53"];
-
-var expandBox = anime({
-  targets: '.expand-js-1',
-  duration: 1000,
-  width: '100%',
-  easing: 'easeInOutSine',
-  skewX: [ anime.stagger([-120, -20]) , 0 ],
-  delay: anime.stagger(300),
-  background: function(el, i, l){
-    return colors[i];
-  },
-});
-
-function runbox2() {
-
-  console.log("the function has run");
-
-  var expandBox2 = anime({
-    targets: '.expand-js-2',
-    duration: 1500,
-    easing: 'easeInOutSine',
-    skewY: [-90,0],
-    opacity: [0,1],
-    delay: anime.stagger(400),
-    background: function(el, i, l){
-      return colors[i];
-    },
-    autoplay: true,
-  });
-
-}
-
-//document.getElementById('gobutton').onmouseover = mytest();
-document.getElementById('gobutton').addEventListener("mouseover", vibrate );
-
-function mytest() {
-  console.log("success");
-}
-
-function vibrate() {
-
-  console.log("mouse-over");
-
-  anime({
-    targets: '#gobutton',
-    translateX: ['-.25rem', '.25rem'],
-    duration: 50,
-    direction: 'alternate',
-    loop: 10,
-    easing: 'easeOutBack',
-  });
-  
-}
